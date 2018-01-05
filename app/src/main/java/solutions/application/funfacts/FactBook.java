5eff52c6bb52d51a3db55cfde1d9cb6b37b3d185
Marissa Nicasio <mnicasio@Marissas-MBP.attlocal.net>
2018-01-05 02:04:39 -0600

    fact revision

--- app/src/main/java/solutions/application/funfacts/FactBook.java
+++ app/src/main/java/solutions/application/funfacts/FactBook.java
@@ -10,7 +10,7 @@ class FactBook {
             "Ostriches can run faster than horses.",
             "Olympic gold medals are actually made mostly of silver.",
             "You are born with 300 bones; by the time you are an adult you will have 206.",
-            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "It takes 8 minutes and 20 seconds for light from the Sun to reach Earth.",
             "Some bamboo plants can grow almost a meter in just one day.",
             "The state of Florida is bigger than England.",
             "Some penguins can leap 2-3 meters out of the water.",
